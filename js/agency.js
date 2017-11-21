dc0f2abc7c955f22891c66a06711715046035ad4
David Veytsman <dveyts@yahoo.com>
2017-11-21 09:06:59 -0500

    changed logic to be able to view the profile picture

--- js/agency.js
+++ js/agency.js
@@ -1,3 +1,6 @@
+
+
+
 (function($) {
   "use strict"; // Start of use strict
   // Smooth scrolling using jQuery easing
---
+++ js/extra.js
@@ -0,0 +1,18 @@
+var pic = document.querySelector("#profile-pic");
+
+  window.onresize = function(){
+    console.log(window.outerWidth);
+    if(window.outerWidth < 768){
+      pic.classList.add("profile-pic");
+    } else {
+      pic.classList.remove("profile-pic");
+    }
+  };
+  window.onload = function(){
+    console.log(window.outerWidth);
+    if(window.outerWidth < 768){
+      pic.classList.add("profile-pic");
+    } else {
+      pic.classList.remove("profile-pic");
+    }
+  };
